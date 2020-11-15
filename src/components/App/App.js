4c64a9e2db68ad8bdab95d9f169e8345fe153791
haley-r <haleyryan77@gmail.com>
2020-11-14 21:37:30 -0600

    basic gameboard and win conditions

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import GameBoard from '../GameBoard/GameBoard'
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Obsession</h1>
+      </header>
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
