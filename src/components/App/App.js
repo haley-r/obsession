3f9ba27d0262e98bbb27f191e4983a70984a847e
haley-r <haleyryan77@gmail.com>
2020-11-15 20:23:38 -0600

    minor App.js cleanup. moving onto gameboard/redux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import Game from '../Game/Game'
 import React, { useEffect } from 'react';
-
 // import './App.css';
 
 const URL = 'ws://localhost:5000/websocket'
@@ -15,24 +14,22 @@ function App() {
       // on connecting, do nothing but log it to the console
       console.log('ws connected in App.js')
     }
-
     ws.onmessage = event => {
       // on receiving a message, update things based on whats in the message
       const message = JSON.parse(event.data)
-      console.log('message is: ', message);
+      // then do something with the message
+      console.log('message received in App.js is: ', message);
       // this.addMessage(message)
     }
-
     ws.onclose = () => {
       console.log('disconnected')
       // automatically try to reconnect on connection loss
+      // obviously would need to rewrite for funcitonal component, so do that later
       // this.setState({
       //     ws: new WebSocket(URL),
       // })
     }
   });
-
-
 
   return (
       <div className="App">
