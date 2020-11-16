c2721dd520f3c62b343e06f4f223272f27af48fd
haley-r <haleyryan77@gmail.com>
2020-11-15 20:18:57 -0600

    ws stuff added to App.js, not doing anything but it didn't break yet

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,36 @@
 import Game from '../Game/Game'
-
+import React, { useEffect } from 'react';
 
 // import './App.css';
+
+const URL = 'ws://localhost:5000/websocket'
+
+
 function App() {
 
+  const ws = new WebSocket(URL)
 
-// put websocket stuff here
+    useEffect(() => {
+    ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log('ws connected in App.js')
+    }
+
+    ws.onmessage = event => {
+      // on receiving a message, update things based on whats in the message
+      const message = JSON.parse(event.data)
+      console.log('message is: ', message);
+      // this.addMessage(message)
+    }
+
+    ws.onclose = () => {
+      console.log('disconnected')
+      // automatically try to reconnect on connection loss
+      // this.setState({
+      //     ws: new WebSocket(URL),
+      // })
+    }
+  });
 
 
 
