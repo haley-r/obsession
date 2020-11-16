02dae604c388b0eb81277ec44857bd11014cd311
haley-r <haleyryan77@gmail.com>
2020-11-15 21:07:00 -0600

    starting to set up redux. Game.js is set up to dispatch but nothing is recieving the dispatches

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import GameBoard from '../GameBoard/GameBoard';
 
@@ -7,25 +7,24 @@ const URL = 'ws://localhost:5000/websocket'
 
 
 function Game() {
-    // const [roomName, setRoomName] = useState("");
-    // const [roomId, setRoomId] = useState("");
-    // const currentRoom = useSelector(state => state.room);
-    const [currentRoom, setCurrentRoom] = useState('hi');
+    const [createRoomInput, setCreateRoomInput] = useState("");
+    const [joinRoomInput, setJoinRoomInput] = useState("");
+    const currentRoom = useSelector(state => state.room);
 
-//     const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
     return (
         <>
             {!currentRoom && 
                 <div>
-                    <h1>Welcome to Obsession Online!</h1>
+                    <h3>do u want to start or join a game?</h3>
                     <div>
-                        <input type="text" placeholder="whatever you want" />
-                        <button>start a game</button>
+                        <input type="text" placeholder="name your room" value={createRoomInput} onChange={(event) => setCreateRoomInput(event.target.value)} />
+                        <button onClick={() => dispatch({ type: "CREATE_ROOM", createRoomInput })}>create room</button>
                     </div>
                     <div>
-                        <input type="text" placeholder="your friends game id" />
-                        <button>join a game</button>
+                        <input type="text" placeholder="enter existing room name" value={joinRoomInput} onChange={(event) => setJoinRoomInput(event.target.value)}/>
+                        <button onClick={() => dispatch({ type: "JOIN_ROOM", joinRoomInput })}>Join</button>
                     </div>
                 </div>
             }
@@ -37,34 +36,3 @@ function Game() {
 }
 
 export default Game;
-
-
-
-
-
-
-//     return (
-//         <>
-//             {!currentRoom &&
-//                 <div className="create">
-//                     <div>
-//                         <span>Create new room</span>
-//                         <input type="text" placeholder="Room name" value={roomName} onChange={(e) => setRoomName(e.target.value)} />
-//                         <button onClick={() => dispatch({ type: "CREATE_ROOM", roomName })}>Create</button>
-//                     </div>
-//                     <div>
-//                         <span>Join existing room</span>
-//                         <input type="text" placeholder="Room code" value={roomId} onChange={(e) => setRoomId(e.target.value)} />
-//                         <button onClick={() => dispatch({ type: "JOIN_ROOM", roomId })}>Join</button>
-//                     </div>
-//                 </div>
-//             }
-
-//             {currentRoom &&
-//                 <ChatRoom />
-//             }
-//         </>
-//     );
-// }
-
-// export default HomeComponent;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,87 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import axios from 'axios';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+
+// I don't know if this is needed/if http will be used for this project? 
+// but leaving it in just in case/to remind myself
+const API_BASE = "http://localhost:5000";
+
+//make a game reducer that holds room name, username, and the state of the board
+//these seem like they should be broken up into multiple reducers
+//but anyway
+const gameReducer = (state = { 
+                              room: null, 
+                              boardState: {}, 
+                              username: null 
+                            }, action) => {
+  switch (action.type) {
+    case "CREATE_ROOM_SUCCESS":
+      state.room = action.payload;
+      break;
+    case "JOIN_ROOM_SUCCESS":
+      state.room = action.payload;
+      break;
+    case "SET_USERNAME":
+      console.log('in set_username case with action: ', action);
+      state.username = action.username;
+      break;
+    // case "UPDATE_CHAT_LOG":
+    //   if (state.room !== null && action.update.roomId === state.room.id) {
+    //     state.chatLog = [...state.chatLog, action.update.data];
+    //   }
+    //   break;
+  }
+  return state
+}
+
+// sagas
+function* createRoom(action) {
+  try {
+    const response = yield axios.get(`${API_BASE}/room?name=${action.roomName}`)
+    yield put({ type: "CREATE_ROOM_SUCCESS", payload: response.data });
+  } catch (error) {
+    yield put({ type: "CREATE_ROOM_ERROR", error });
+  }
+}
+
+function* joinRoom(action) {
+  try {
+    const response = yield axios.get(`${API_BASE}/room/${action.roomId}`)
+    yield put({ type: "JOIN_ROOM_SUCCESS", payload: response.data });
+  } catch (error) {
+    yield put({ type: "JOIN_ROOM_ERROR", error });
+  }
+}
+
+
+function* watcherSaga() {
+  yield takeEvery('CREATE_ROOM', createRoom);
+  yield takeEvery('JOIN_ROOM', joinRoom);
+}
+
+// make instance of saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+//make a store
+const storeInstance = createStore(
+  gameReducer,
+  applyMiddleware(logger, sagaMiddleware),
 );
 
+//run watcher saga
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
